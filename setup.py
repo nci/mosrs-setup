a3a361540337d0cd78dadb6d679fe1610ea5b780
Scott Wales <scott.wales@unimelb.edu.au>
2017-06-21 16:37:42 +1000

    Update version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
         name     = 'mosrs',
-        version  = '0.1.3',
+        version  = '0.2.0',
         packages = find_packages(),
         entry_points = {
             'console_scripts': [
