157b356d2ff1982049ef4a483b6a614a9a271bef
Scott Wales <scott.wales@unimelb.edu.au>
2018-03-27 10:05:49 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
         name     = 'mosrs',
-        version  = '0.2.3',
+        version  = '0.3.0',
         packages = find_packages(),
         install_requires = [
             'python-ldap',
