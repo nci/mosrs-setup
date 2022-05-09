5d324b8218fe9192c731a37f3ee7585880c20d8f
Paul Leopardi <paul.leopardi@anu.edu.au>
2022-05-09 15:30:57 +1000

    Cache NCI web credentials

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
         name     = 'mosrs',
-        version  = '0.4.0',
+        version  = '0.4.1',
         packages = find_packages(),
         install_requires = [
             'python-ldap <= 3.3.1',
@@ -32,6 +32,7 @@ setup(
                 'mosrs-auth  = mosrs.auth:main',
                 'mosrs-setup = mosrs.setup:main',
                 'access-auth = mosrs.access:main',
+                'nciws-auth  = mosrs.nciws:main',
                 'accessdev-auth = mosrs.accessdev:main',
                 'accesssvn-setup = mosrs.setup:accesssvn_setup',
                 ]
