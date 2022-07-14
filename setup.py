31bb149b035f1b8c88941628ced96b186a0cd8ba
Paul Leopardi <paul.leopardi@anu.edu.au>
2022-07-14 14:51:57 +1000

    Bump version to 0.4.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
         name     = 'mosrs',
-        version  = '0.4.2',
+        version  = '0.4.3',
         packages = find_packages(),
         install_requires = [
             'python-ldap <= 3.3.1',
