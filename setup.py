6d16c7b4edfdad809e6cbf43c0c910efacfdce8f
Paul Leopardi <paul.leopardi@anu.edu.au>
2022-11-01 10:35:53 +1100

    Clean up

--- setup.py
+++ setup.py
@@ -20,30 +20,30 @@ limitations under the License.
 from setuptools import setup, find_packages
 
 setup(
-        name     = 'mosrs',
-        version  = '0.7.0',
-        description = 'Cache credentials for NCI users of MOSRS',
-        license = 'Apache License, Version 2.0',
-        packages = find_packages(),
-        classifiers = [
-          'Development Status :: 4 - Beta',
-          'Environment :: Console',
-          'License :: OSI Approved :: Apache Software License',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: System :: Systems Administration :: Authentication/Directory',
+    name='mosrs',
+    version='0.7.0',
+    description='Cache credentials for NCI users of MOSRS',
+    license='Apache License, Version 2.0',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
+    author='Scott Wales',
+    maintainer='National Computational Infrastructure',
+    install_requires=[
+        'python-ldap <= 3.3.1',
+        'certifi <= 2021.10.8',
+        'requests < 2.28',
         ],
-        author = "Scott Wales",
-        maintainer = "National Computational Infrastructure",
-        install_requires = [
-            'python-ldap <= 3.3.1',
-            'certifi <= 2021.10.8',
-            'requests < 2.28',
-            ],
-        entry_points = {
-            'console_scripts': [
-                'mosrs-auth  = mosrs.auth:main',
-                'mosrs-setup = mosrs.setup:main',
-                ]
-            }
-        )
+    entry_points={
+        'console_scripts': [
+            'mosrs-auth=mosrs.auth:main',
+            'mosrs-setup=mosrs.setup:main',
+            ]
+        }
+    )
