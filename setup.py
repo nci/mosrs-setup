745c678cd9e2def7d8f316a0db9e9999eb350484
Paul Leopardi <paul.leopardi@anu.edu.au>
2023-03-08 15:57:26 +1100

    Bump version to 0.9.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mosrs',
-    version='0.9.3',
+    version='0.9.4',
     description='Cache credentials for NCI users of MOSRS',
     license='Apache License, Version 2.0',
     packages=find_packages(),
